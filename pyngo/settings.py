f51cf6f2aaa1cc024be94f2b4a5aa8d5f7df79db
dwiga <dwi.hyuga@gmail.com>
2017-08-04 21:21:27 +0700

    sampai tahap 2

--- pyngo/settings.py
+++ pyngo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djaton.apps.DjatonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
